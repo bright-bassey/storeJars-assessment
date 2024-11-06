421cd38dfcf2491a5b4b6583922e4511f151aac6
unknown <brightbassey1819@gmail.com>
2024-11-06 08:22:47 +0000

    npx hardhat compile

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,17 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
+import "@typechain/hardhat";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6"
+  },
   networks: {
     sepolia: {
       url: process.env.ETHEREUM_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111  // Sepolia's chain ID
+      chainId: 11155111
     }
   }
 };
